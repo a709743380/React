7cb901562cc5544daa5e4bc34e519397bbcbdb22
a709743380 <84056966+a709743380@users.noreply.github.com>
2023-11-17 22:02:28 +0800

    更新

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,21 +1,46 @@
 import New_posts from "./Pages/new_posts";
 import Signin from "./Pages/Signin";
 import MemberLayout from "./MemberLayout";
+import ViewPost from "./Pages/ViewPost";
+import PostLayout from "./PostLayout";
+
 const AppRoutes = [
   {
-    path: "React/Signin",
+    path: "/React",
+    element: "",
+  },
+  {
+    path: "/React/Signin",
     element: <Signin />,
   },
   {
-    path: "React/new_posts",
+    path: "/React/new_posts",
     element: <New_posts />,
   },
   {
-    name:"",
-    path: "React/my/",
-    element:<MemberLayout />,
-  }
-
+    path: "/React/my/",
+    element: <MemberLayout />,
+  },
+  {
+    path: "/React/Posts",
+    element: <PostLayout element={<ViewPost />} />,
+  },
+  {
+    path: "/React/ViewPost/:paramId",
+    element: <PostLayout element={<ViewPost />} />,
+  },
+  {
+    path: "/React/my/posts",
+    element: <MemberLayout element={"我的文章"} />,
+  },
+  {
+    path: "/React/my/collections",
+    element: <MemberLayout element={"我的收藏"} />,
+  },
+  {
+    path: "/React/my/setting",
+    element: <MemberLayout element={"我的設定"} />,
+  },
 ];
 
 export default AppRoutes;
---
+++ src/GetPost.js
@@ -0,0 +1,34 @@
+import { Item } from "semantic-ui-react";
+
+
+function GetPost() {
+  return (
+    <Item
+      key={postItemData.id}
+      as={Link}
+      to={`/React/ViewPost/${postItemData.id}`}
+    >
+      <Item.Image src={postItemData?.ImgUrl || "../../Image/unfind.png"} />
+      <Item.Content>
+        <Item.Meta>
+          {postItemData.author?.photoUrl ? (
+            <Item.Image
+              src={postItemData.author?.photoUrl || "../../Image/unfind.png"}
+            />
+          ) : (
+            <Icon name="user circle"></Icon>
+          )}
+          {postItemData?.topic} {postItemData.author?.displayName || "使用者"}
+        </Item.Meta>
+        <Item.Header>{postItemData?.title}</Item.Header>
+        <Item.Description>{postItemData?.content}</Item.Description>
+        <Item.Extra>
+          留言 {postItemData?.commentCount || 0} 讚{" "}
+          {postItemData.liked?.length || 0}{" "}
+        </Item.Extra>
+      </Item.Content>
+    </Item>
+  );
+}
+
+export default GetPost;
